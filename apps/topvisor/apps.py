fe72af2119d7d97481bf3f59603d3211b9a42cb1
gptchatseo1-blip <gptchat.seo.1@gmail.com>
2026-08-02 15:51:52 +0300

    Add apps/topvisor/apps.py for depth-aware Topvisor integration

---
+++ apps/topvisor/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TopvisorConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.topvisor"
+    verbose_name = "Topvisor"
